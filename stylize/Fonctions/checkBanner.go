3ff819854ad552266b3f4397658e6c2fefa147e3
Ayoub Benramdane <ayoubbenramdane05@gmail.com>
2024-07-18 04:21:47 +0100

    new

---
+++ stylize/Fonctions/checkBanner.go
@@ -0,0 +1,29 @@
+package Fonctions
+
+import (
+	"os"
+	"strings"
+)
+
+func AsciiArt(Banner, str string) string {
+	file, err := os.ReadFile("banner/" + Banner)
+	if err != nil || !CheckBanner(file, str) {
+		os.Exit(0)
+	}
+	fileFinal := strings.ReplaceAll(string(file), "\r", "")
+	return PrintAsciiArt(string(fileFinal), str)
+}
+
+func CheckBanner(Banner []byte, str string) bool {
+	var maxAscii rune
+	for _, c := range str {
+		if maxAscii == 0 && c >= 32 && c <= 126 {
+			maxAscii = c
+		} else if c > maxAscii && c >= 32 && c <= 126 {
+			maxAscii = c
+		}
+	}
+	Req_Len := int((maxAscii - 31) * 9)
+	Len_File := len(strings.Split(string(Banner), "\n"))
+	return Req_Len < Len_File
+}
---
+++ stylize/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"Fonctions/Fonctions"
+)
+
+type ascii struct {
+	Result string
+	Banner string
+	Text   string
+}
+
+var q ascii
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("POST /", ascii_Art)
+	fmt.Println("Server is Runing...")
+	http.ListenAndServe(":8010", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Status Not Found 404", http.StatusNotFound)
+		return
+	}
+	temp, err := template.ParseFiles("src/index.html")
+	if err != nil {
+		http.Error(w, "Status Internal Server Error 500", http.StatusInternalServerError)
+		return
+	}
+	temp.Execute(w, q)
+}
+
+func ascii_Art(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Status Method Not Allowed 405", http.StatusMethodNotAllowed)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Status Bad Request 400", http.StatusBadRequest)
+		return
+	}
+	q.Text = r.Form.Get("text")
+	q.Banner = r.Form.Get("banner")
+	if q.Banner != "standard.txt" && q.Banner != "shadow.txt" && q.Banner != "thinkertoy.txt" {
+		http.Error(w, "Status Bad Request 400", http.StatusBadRequest)
+		return
+	}
+	q.Result = Fonctions.AsciiArt(q.Banner, q.Text)
+	if len(q.Text) > 1 && q.Text[0:2] == "\r\n" {
+		q.Text = "\r\n" + q.Text
+	}
+	temp, err := template.ParseFiles("src/index.html")
+	if err != nil {
+		http.Error(w, "Status Internal Server Error 500", http.StatusInternalServerError)
+		return
+	}
+	temp.Execute(w, q)
+}
